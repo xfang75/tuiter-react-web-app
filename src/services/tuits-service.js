426c17943423aa19e12b9f8feeb92a498c1b8f6a
Xun Fang <xunfang@Xuns-MacBook-Air.local>
2022-11-20 20:11:24 -0800

    remove extra lines

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-//const TUITS_API = 'http://localhost:4000/api/tuits'
+
 //const TUITS_API = 'https://tuiter-node-server-app-xfang75.herokuapp.com/api/tuits';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
